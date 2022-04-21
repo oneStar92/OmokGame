cc1448e330de30e91a564d4c5264822bb5f733b0
Onestar <onestar7984@gmail.com>
2022-04-21 19:18:24 +0900

    first commit

---
+++ OmokGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OmokGame
+//
+//  Created by 윤일성 on 2022/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Omokgame Project
